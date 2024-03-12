2b2fe06d1450c36e94b0f89693d5444a5e6319fd
chris-vaz <chrisvas2001@gmail.com>
2024-03-12 14:52:31 +0300

    Updated login page component

--- mind-canvas/src/app/(auth)/login/page.tsx
+++ mind-canvas/src/app/(auth)/login/page.tsx
@@ -1,71 +1,141 @@
 'use client';
-import React, { useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { SubmitHandler, useForm } from 'react-hook-form'
-
-// Importing necessary Zod packages
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import * as z from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod'
-
+import { zodResolver } from '@hookform/resolvers/zod';
 import { FormSchema } from '@/lib/types';
-import { Form, FormControl, FormDescription, FormField, FormItem } from '@/components/ui/form';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
 import Link from 'next/link';
 import Image from 'next/image';
-import Logo from '../../../../public/cypresslogo.svg'
-import { Input } from 'postcss';
+import Logo from '../../../../public/cypresslogo.svg';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+// import Loader from '@/components/global/Loader';
+// import { Separator } from '@/components/ui/separator';
+// import { actionLoginUser } from '@/lib/server-actions/auth-actions';
 
 const LoginPage = () => {
-    const router = useRouter();
-    const [submitError, setSubmitError] = useState('');
+  const router = useRouter();
+  const [submitError, setSubmitError] = useState('');
 
-    // z.infer - extracts the inferred type
-    // In the case of FormSchema, { email: string }{ pw: string } 
+  const form = useForm<z.infer<typeof FormSchema>>({
+    mode: 'onChange',
+    resolver: zodResolver(FormSchema),
+    defaultValues: { email: '', password: '' },
+  });
 
-    // extract the inferred type like this
-    // type Dog = z.infer<typeof Dog>;
+  const isLoading = form.formState.isSubmitting;
 
-    // // equivalent to:
-    // type Dog = {
-    //   name: string;
-    //   age: number;
-    // };
+//   const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = async (
+//     formData
+//   ) => {
+//     const { error } = await actionLoginUser(formData);
+//     if (error) {
+//       form.reset();
+//       setSubmitError(error.message);
+//     }
+//     router.replace('/dashboard');
+//   };
 
-    const form = useForm<z.infer<typeof FormSchema>>({
-        mode: 'onChange',
-        resolver: zodResolver(FormSchema), // This integrates Zod validation with react-hook-form. It uses the provided FormSchema to define the validation rules for your form fields.
-        defaultValues: { email: '', password: '' }, // This sets the initial values for your form's email and password fields as empty strings.
-    });
+  return (
+    <Form {...form}>
+      <form
+        onChange={() => {
+          if (submitError) setSubmitError('');
+        }}
+        // onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full sm:justify-center sm:w-[400px] space-y-6 flex flex-col"
+      >
+        <Link
+          href="/"
+          className="
+          w-full
+          flex
+          justify-left
+          items-center"
+        >
+          <Image
+            src={Logo}
+            alt="cypress Logo"
+            width={50}
+            height={50}
+          />
+          <span
+            className="font-semibold
+          dark:text-white text-4xl first-letter:ml-2"
+          >
+            cypress.
+          </span>
+        </Link>
+        <FormDescription
+          className="
+        text-foreground/60"
+        >
+          An all-In-One Collaboration and Productivity Platform
+        </FormDescription>
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {submitError && <FormMessage>{submitError}</FormMessage>}
+        {/* <Button
+          type="submit"
+          className="w-full p-6"
+          size="lg"
+          disabled={isLoading}
+        >
+          {!isLoading ? 'Login' : <Loader />}
+        </Button> */}
+        <span className="self-container">
+          Dont have an account?{' '}
+          <Link
+            href="/signup"
+            className="text-primary"
+          >
+            Sign Up
+          </Link>
+        </span>
+      </form>
+    </Form>
+  );
+};
 
-    const isLoading = form.formState.isSubmitting;
-
-    const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = async (formData) => {
-
-    };
-
-
-    return (
-        <Form {...form}>
-            <form
-                onChange={() => {
-                    if (submitError) setSubmitError('');
-                }}
-                onSubmit={form.handleSubmit(onSubmit)}
-                className='w-full sm:justify-center sm:w-[400px] space-y-6 flex flex-column'>
-                <Link href="/" className='w-full flex justify-left items-center'>
-                    <Image src={Logo} alt="MindCanvas Logo" width={50} height={50} />
-                    <span className='font-semibold text-4xl first-letter:ml-2'>Mind Canvas</span>
-                </Link>
-                <FormDescription className='text-foreground/60 '></FormDescription>
-                <FormField>
-                    <FormItem>
-                        <FormControl>
-                            <Input></Input>
-                        </FormControl>
-                    </FormItem>
-                </FormField>
-            </form>
-        </Form>
-    )
-}
-
-export default LoginPage
+export default LoginPage;
