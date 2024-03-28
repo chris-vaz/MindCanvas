365cbc465acda2f40c26fce936e1b24102f2a976
chris-vaz <chrisvas2001@gmail.com>
2024-03-28 11:13:44 +0300

    Added sidebar

---
+++ mind-canvas/src/components/sidebar/sidebar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface Sidebar {
+    params: { workspaceId: string };
+    className?: string
+}
+
+const Sidebar = () => {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
