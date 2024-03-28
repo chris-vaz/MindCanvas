e252aba929272e85412190d99f097bc8f8a08217
chris-vaz <chrisvas2001@gmail.com>
2024-03-28 11:58:31 +0300

    Updated Sidebar layout

--- mind-canvas/src/app/(main)/dashboard/[workspaceId]/layout.tsx
+++ mind-canvas/src/app/(main)/dashboard/[workspaceId]/layout.tsx
@@ -1,18 +1,25 @@
 // import { SubscriptionModalProvider } from '@/lib/providers/subscription-modal-provider';
 // import { getActiveProductsWithPrice } from '@/lib/supabase/queries';
+import Sidebar from '@/components/sidebar/sidebar';
 import React from 'react';
 
 interface LayoutProps {
-  children: React.ReactNode;
-  params: any;
+    children: React.ReactNode;
+    params: any;
 }
 
 const Layout: React.FC<LayoutProps> = async ({ children, params }) => {
-  return (
-    <div className="">
-      {children}
-    </div>
-  );
+    return (
+        <main className="flex overflow-hidden h-screen w-screen">
+            <Sidebar params={params} />
+            {/* <MobileSidebar> */}
+                <Sidebar params={params} className="w-screen inline-block sm:hidden" />
+            {/* </MobileSidebar> */}
+            <div className="dark:boder-Neutrals-12/70 border-l-[1px] w-full relative overflow-scroll">
+                {children}
+            </div>
+        </main>
+    );
 };
 
 export default Layout;
