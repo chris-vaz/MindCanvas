2bfd2e44c37c6971e09b35d9143963671b05443a
chris-vaz <chrisvas2001@gmail.com>
2024-04-03 14:09:53 +0300

    Updated migration files

---
+++ mind-canvas/migrations/0004_huge_gamora.sql
@@ -0,0 +1,137 @@
+DO $$ BEGIN
+ CREATE TYPE "aal_level" AS ENUM('aal1', 'aal2', 'aal3');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "action" AS ENUM('INSERT', 'UPDATE', 'DELETE', 'TRUNCATE', 'ERROR');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "code_challenge_method" AS ENUM('s256', 'plain');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "equality_op" AS ENUM('eq', 'neq', 'lt', 'lte', 'gt', 'gte', 'in');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "factor_status" AS ENUM('unverified', 'verified');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "factor_type" AS ENUM('totp', 'webauthn');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "key_status" AS ENUM('default', 'valid', 'invalid', 'expired');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "key_type" AS ENUM('aead-ietf', 'aead-det', 'hmacsha512', 'hmacsha256', 'auth', 'shorthash', 'generichash', 'kdf', 'secretbox', 'secretstream', 'stream_xchacha20');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "pricing_plan_interval" AS ENUM('day', 'week', 'month', 'year');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "pricing_type" AS ENUM('one_time', 'recurring');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "subscription_status" AS ENUM('trialing', 'active', 'canceled', 'incomplete', 'incomplete_expired', 'past_due', 'unpaid');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "customers" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"stripe_customer_id" text
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "prices" (
+	"id" text PRIMARY KEY NOT NULL,
+	"product_id" text,
+	"active" boolean,
+	"description" text,
+	"unit_amount" bigint,
+	"currency" text,
+	"type" "pricing_type",
+	"interval" "pricing_plan_interval",
+	"interval_count" integer,
+	"trial_period_days" integer,
+	"metadata" jsonb
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "products" (
+	"id" text PRIMARY KEY NOT NULL,
+	"active" boolean,
+	"name" text,
+	"description" text,
+	"image" text,
+	"metadata" jsonb
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "profiles" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"updated_at" timestamp with time zone,
+	"full_name" text,
+	"avatar_url" text,
+	"website" text,
+	"email" text,
+	"billing_address" jsonb,
+	"payment_method" jsonb
+);
+--> statement-breakpoint
+ALTER TABLE "collaborators" DROP CONSTRAINT "collaborators_user_id_users_id_fk";
+--> statement-breakpoint
+ALTER TABLE "files" DROP CONSTRAINT "files_folder_id_folders_id_fk";
+--> statement-breakpoint
+ALTER TABLE "workspaces" ALTER COLUMN "created_at" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "collaborators" ADD CONSTRAINT "collaborators_workspace_id_user_id_pk" PRIMARY KEY("workspace_id","user_id");--> statement-breakpoint
+ALTER TABLE "folders" ADD COLUMN "folder_id" uuid DEFAULT gen_random_uuid() NOT NULL;--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "collaborators" ADD CONSTRAINT "collaborators_user_id_profiles_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."profiles"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "files" ADD CONSTRAINT "files_folder_id_folders_folder_id_fk" FOREIGN KEY ("folder_id") REFERENCES "public"."folders"("folder_id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "workspaces" ADD CONSTRAINT "workspaces_workspace_owner_profiles_id_fk" FOREIGN KEY ("workspace_owner") REFERENCES "public"."profiles"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+ALTER TABLE "collaborators" DROP COLUMN IF EXISTS "id";--> statement-breakpoint
+ALTER TABLE "folders" DROP COLUMN IF EXISTS "id";--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "prices" ADD CONSTRAINT "prices_product_id_products_id_fk" FOREIGN KEY ("product_id") REFERENCES "public"."products"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
